9e5de747d54c87c8aff4a3ad40f19787f24cd212
helloprof <suluxan.seneca@gmail.com>
2023-02-03 19:03:47 -0500

    server.js with /genres, /videos, and / and also using the videoService functions/module

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const videoService = require("./videoService")
+
+const HTTP_PORT = process.env.PORT || 8080
+
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname,"/views/index.html"))
+})
+
+app.get("/genres", (req,res) => {
+  videoService.getGenres().then((genres) => {
+    res.json(genres)
+  }).catch((err) => {
+    console.log(err)
+    res.send(err)
+  })
+})
+
+app.get("/videos", (req,res) => {
+  videoService.getVideos().then((videos) => {
+    res.json(videos)
+  }).catch((err) => {
+    console.log(err)
+    res.send(err)
+  })
+})
+
+app.use((req, res) => {
+  res.status(404).send("Page Not Found")
+})
+
+videoService.initialize().then(() => {
+  app.listen(HTTP_PORT, onHttpStart)
+})
