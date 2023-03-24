8a84e2f1f9f243aef73d93ca9f17a37b0eacbeee
helloprof <suluxan.seneca@gmail.com>
2023-03-24 18:32:44 -0400

    new auth service with mongo db schema, connection, register and login functions

---
+++ authService.js
@@ -0,0 +1,76 @@
+var mongoose = require("mongoose")
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    "username": {
+        type: String,
+        unique: true
+    },
+    "password": String,
+    "email": String,
+    "loginHistory": [{
+        "dateTime": Date,
+        "userAgent": String
+    }]
+})
+
+let User; // to be defined on new connection (see initialize)
+
+module.exports.initialize = function () {
+    return new Promise(function (resolve, reject) {
+        let db = mongoose.createConnection(process.env.MONGO_URI_STRING);
+
+        db.on('error', (err)=>{
+            reject(err); // reject the promise with the provided error
+        });
+        db.once('open', ()=>{
+           User = db.model("users", userSchema);
+           console.log("MONGO DB LOADED")
+           resolve();
+        });
+    });
+};
+
+
+module.exports.registerUser = function(userData) {
+    return new Promise((resolve, reject) => {
+        if (userData.password != userData.password2) {
+            reject("PASSWORDS DO NOT MATCH")
+        } else {
+            let newUser = new User(userData)
+            newUser.save().then(() => {
+                resolve()
+            }).catch((err) => {
+                if(err.code == 11000) {
+                    console.log("USERNAME TAKEN")
+                    reject("USERNAME ALREADY TAKEN!")
+                } else {
+                    reject(err)
+                }
+            })
+        }
+    })
+}
+
+module.exports.loginUser = function(userData) {
+    return new Promise((resolve, reject) => {
+        User.findOne({username: userData.username})
+        .exec()
+        .then((user) => {
+            if(!user) {
+                reject("USER NOT FOUND!")
+            } else {
+                if (userData.password == user.password) {
+                    resolve()
+                } else {
+                    console.log(userData.password, user.password)
+                    reject("INCORRECT PASSWORD!")
+                }
+            }
+        }).catch((err) => {
+            reject("DATABASE ERROR")
+            console.log(err)
+        })
+    })
+}
+
